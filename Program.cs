ade6397836d71c48f60359f96b04c1eda32254dc
mcantiere <matheus.cantieree@gmail.com>
2026-03-11 22:26:55 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int Opcao = 0;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("Calculadora (Adição / Subtração / Multiplicação / Divisão)");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+
+            Console.Write("\nOpção: ");
+            Opcao = Convert.ToInt32(Console.ReadLine());
+
+            if (Opcao >= 1 && Opcao <= 4)
+            {
+                Console.Write("Digite o primeiro número: ");
+                double Numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite o segundo número: ");
+                double Numero2 = Convert.ToDouble(Console.ReadLine());
+
+                double Resultado = 0;
+
+                switch (Opcao)
+                {
+                    case 1:
+                        Resultado = Numero1 + Numero2;
+                        Console.WriteLine($"\nResultado: {Resultado}");
+                        break;
+
+                    case 2:
+                        Resultado = Numero1 - Numero2;
+                        Console.WriteLine($"\nResultado: {Resultado}");
+                        break;
+
+                    case 3:
+                        Resultado = Numero1 * Numero2;
+                        Console.WriteLine($"\nResultado: {Resultado}");
+                        break;
+
+                    case 4:
+                        if (Numero2 == 0)
+                        {
+                            Console.WriteLine("\nErro: divisão por zero não é permitida.");
+                        }
+                        else
+                        {
+                            Resultado = Numero1 / Numero2;
+                            Console.WriteLine($"\nResultado: {Resultado}");
+                        }
+                        break;
+                }
+            }
+            else if (Opcao != 5)
+            {
+                Console.WriteLine("\nOpção inválida!");
+            }
+
+            if (Opcao != 5)
+            {
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+
+        } while (Opcao != 5);
+
+        Console.WriteLine("\nPrograma encerrado.");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Caculadora-C-
-Calculadora usando C# feito para entrega do CP01
+# Calculadora Console C#
+ 
+Checkpoint 1 – Aplicação Console em C#
+ 
+## Integrantes
+- Matheus Cantiere RM558479
+- Vinicius Castro RM556137
+- Guilherme Barbiero RM555185
+- Marco Antonio Gonçalves RM556818
+- Camila Mie Takara RM555418
+ 
+## Funcionalidades
+Calculadora com operações:
+ 
+- Adição
+- Subtração
+- Multiplicação
+- Divisão
+ 
+## Prints
+ 
+### Menu
+![alt text](/assets/image.png)
+ 
+### Adição
+![alt text](/assets/image-1.png)
+ 
+### Subtração
+![alt text](/assets/image-2.png)
+ 
+### Multiplicação
+![alt text](/assets/image-3.png)
+
+Multiplicação por zero:
+![alt text](/assets/image-6.png)
+ 
+### Divisão
+![alt text](/assets/image-4.png)
+
+Divisão por zero:
+![alt text](/assets/image-5.png)
